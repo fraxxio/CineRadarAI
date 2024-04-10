fab0cd31fd76c03a2bb0336051dfdf12f7902011
fraxxio <101990229+fraxxio@users.noreply.github.com>
2024-04-10 17:52:21 +0300

    feat: Add custom Sign in page

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,36 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import { db } from "./db";
 
-export const { handlers, auth } = NextAuth({
+export const authConfig = {
   providers: [GitHub, Google],
   adapter: DrizzleAdapter(db),
-});
+  pages: {
+    signIn: "/signin",
+  },
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const paths = ["/my-list", "/profile"];
+      const isProtected = paths.some((path) =>
+        nextUrl.pathname.startsWith(path),
+      );
+
+      if (isProtected && !isLoggedIn) {
+        const redirectUrl = new URL("api/auth/signin", nextUrl.origin);
+        redirectUrl.searchParams.append("callbackUrl", nextUrl.href);
+        return Response.redirect(redirectUrl);
+      }
+
+      return true;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, auth } = NextAuth(authConfig);
